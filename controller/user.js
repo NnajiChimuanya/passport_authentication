72ef791ea6239159a6c3e97c62e5b2551b386158
Nnaji Chimuanya <nnajichimuanya50@gmail.com>
2022-04-19 20:43:12 +0400

    Added clientis and callbackUrl

--- controller/user.js
+++ controller/user.js
@@ -1,6 +1,7 @@
 const Router = require("express").Router()
 const mongoose = require("mongoose")
-
+const passport = require("passport")
+const GithubStrategy = require("passport-github")
 
 
 mongoose.connect("mongodb://localhost:27017/passport_authentication", (err) => {
@@ -9,5 +10,11 @@ mongoose.connect("mongodb://localhost:27017/passport_authentication", (err) => {
 })
 
 Router.get("/auth", (req, res) => res.send("Authenticattionn"))
+
+
+
+
+
+Router.get("/auth/github")
 
 module.exports = Router
