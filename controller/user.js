49855960471eebb98c1564080a0dd8f9fa968051
Nnaji Chimuanya <nnajichimuanya50@gmail.com>
2022-04-20 11:13:16 +0400

    made necessary file changes and successfully render the displayname of the user and save it t the database

--- controller/user.js
+++ controller/user.js
@@ -4,23 +4,56 @@ const passport = require("passport")
 const GitHubStrategy = require("passport-github")
 const dotenv = require("dotenv")
 const User = require("../model/user")
+const { resetWatchers } = require("nodemon/lib/monitor/watch")
 dotenv.config()
 
 
 
+//database connection
+mongoose.connect("mongodb://localhost:27017/passport_authentication", (err) => {
+    if(err) throw err
+    console.log("Connected to database succeffully")
+})
 
 
+//setting up github strategy
+passport.use(new GitHubStrategy({
+    clientID: process.env.clientID,
+    clientSecret: process.env.clientSecret,
+    callbackURL: process.env.callbackURL
+  },
+  function(accessToken, refreshToken, profile, done) {
+   console.log(profile)
+   User.findOneAndUpdate({name : profile.displayName}, {name : profile.displayName, userId : profile.id},{upsert:true}, (err, user)=> {
+       if(err) throw err
+   })
+   done(null, profile)
+  }
+));
 
+const isAuthenticated = (req, res, next) => {
+    if(req.user) {
+      next()
+    } else {
+      res.redirect("login")
+    }
+}
 
+Router.get("/", isAuthenticated, (req, res) => {
+    console.log(req.user)
+    User.findOne({userId : req.user }, (err, user) => {
+        res.render("home", {user : user})
+    })
+    
+  })
+  
 Router.get("/login", (req, res) => res.render("signup"))
-
-
-
-//Router.get("/", (res, req) => res.send("Authenticated"))
-
+  
 Router.get('/auth/github', passport.authenticate("github"))
-
-Router.get("/auth/github/callback", passport.authenticate("github", { failureRedirect : "/login"}), (req, res) => res.redirect("/ok"))
+  
+Router.get("/auth/github/callback", passport.authenticate("github", { failureRedirect : "/login"}), (req, res) => {
+    res.redirect("/")
+  })
 
 
 
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose")
 const userRouter = require("./controller/user")
 const passport = require("passport")
 const session = require("express-session")
-const GitHubStrategy = require("passport-github")
+
 
 app.set("view engine", "ejs")
 
@@ -26,23 +26,8 @@ app.use(passport.session())
 
 
 
-//app.use("/", userRouter)
+app.use("/", userRouter)
 
-mongoose.connect("mongodb://localhost:27017/passport_authentication", (err) => {
-    if(err) throw err
-    console.log("Connected to database succeffully")
-})
-
-passport.use(new GitHubStrategy({
-    clientID: process.env.clientID,
-    clientSecret: process.env.clientSecret,
-    callbackURL: process.env.callbackURL
-  },
-  function(accessToken, refreshToken, profile, done) {
-   console.log(profile)
-   done(null, profile)
-  }
-));
 
 passport.serializeUser(function(user, done) {
   done(null, user.id);
@@ -53,32 +38,6 @@ passport.deserializeUser(function(id, done) {
     done(null, id);
   
 });
-
-const isAuthenticated = (req, res, next) => {
-  if(req.user) {
-    next()
-  } else {
-    res.redirect("login")
-  }
-}
-
-app.get("/", isAuthenticated, (req, res) => {
-  console.log(req.user)
-  res.render("home")
-})
-
-app.get("/login", (req, res) => res.render("signup"))
-
-app.get('/auth/github', passport.authenticate("github"))
-
-app.get("/auth/github/callback", passport.authenticate("github", { failureRedirect : "/login"}), (req, res) => {
-  res.redirect("/")
-})
-
-
-
-
-
 
 
 app.listen(3000, () => console.log("App is listening at port now"))
