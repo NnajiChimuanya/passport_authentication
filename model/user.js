c7b0d01df8af89cbd7e60ba3ea7392de79a9ce00
Nnaji Chimuanya <nnajichimuanya50@gmail.com>
2022-04-28 16:41:40 +0400

    Implemented the LinkedIn authentication strategy

--- model/user.js
+++ model/user.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema({
     name : String,
-    clientId : Number,
+    clientId : String,
     image : String
 }, {timestamps : true})
 
